b6de54e26a89131692fb81a47d83856dfc37a19a
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-29 19:53:13 -0400

    enviando o segundo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
-import Header from './components/Header.tsx';
+import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
     <main>
       <Header />
+      <Card
+        cityInfo={ {
+          city: '',
+          country: '',
+          imageUrl: '',
+          viseted: undefined,
+        } }
+      />
     </main>
   );
 }
